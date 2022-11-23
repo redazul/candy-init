3b15eb476a1c39f08e54c3f7678c03babef01af1
azuldev <91290695+redazul@users.noreply.github.com>
2022-11-22 21:44:17 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ solana config set --url http://api.devnet.solana.com
 solana-keygen new -o ~/dev.json
 solana config set --keypair ~/dev.json
 cargo install sugar-cli
-solana config set --url <rpc url>
+solana config set --url https://blue-white-log.solana-devnet.quiknode.pro/7385a1ab13d0333b3d529268ceb99e094bffae2d/
